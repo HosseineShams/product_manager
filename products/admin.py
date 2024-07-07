24abe6f46717dd506fcc7efc8c4103ade336dd3b
Hossein <hosseineeshams@gmail.com>
2024-07-07 15:40:48 +0330

    Setup Admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Product, ProductImage
 
-# Register your models here.
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1  # Controls how many extra rows are shown for empty new entries
+    max_num = 5  # Maximum number of images allowed
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'description')  
+    search_fields = ('title', 'description')  
+    inlines = [ProductImageInline] 
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'price', 'description')
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': (),
+        }),
+    )
+
+admin.site.register(Product, ProductAdmin)
